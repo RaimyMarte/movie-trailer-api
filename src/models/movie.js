dc9faf03cf588d05184d4fb4eba28af37565f3f7
Raimy Marte <raimy241@gmail.com>
2023-04-09 11:29:45 -0400

    models movie

--- src/models/movie.js
+++ src/models/movie.js
@@ -4,29 +4,37 @@ const movieSchema = new mongoose.Schema({
     nombre: {
         type: String,
         required: true,
+        trim: true
     },
     generos: {
         type: Array,
+        trim: true
     },
     direccion: {
         type: String,
+        trim: true
     },
     reparto: {
         type: String,
+        trim: true
     },
     sinopsis: {
         type: String,
+        trim: true
     },
     video: {
         type: String,
         required: true,
+        trim: true
     },
     img: {
         type: String,
         required: true,
+        trim: true
     },
     year: {
         type: String,
+        trim: true
     },
 })
 

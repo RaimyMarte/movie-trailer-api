ac57b46c9851051272872fdd1fb38b2becffa66b
Raimy Marte <raimy241@gmail.com>
2023-04-14 12:35:11 -0400

    change to atlad db

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,13 +1,18 @@
-    const mongoose = require('mongoose')
 
-    const host = '127.0.0.1'
-    const port = '27017'
-    const database = 'movie-trailer-api'
+const mongoose = require('mongoose')
 
-    const url = `mongodb://${host}:${port}/${database}`
+const url = `mongodb+srv://raimy241:vI2HWcOgiOXPLa8R@movie-trailer-api.2mddchl.mongodb.net/movie-trailer-api`
 
-    mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to the DB')
     })
+    .catch((e) => {
+        console.log('Error', e)
+    })
+
+
 
--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ const express = require('express')
 const movieRouter = require('./routers/movie')
 const userRouter = require('./routers/user')
 const cors = require('cors')
-
 const serverless = require('serverless-http')
 
 //Data Base
 require('./db/dbConnection')
 
 const app = express()
-const port = 3000
 
 //Config Express
 app.use(cors())
 app.use(express.json())
-app.use('/.netlify/functions/movie', movieRouter)
-app.use('/.netlify/functions/user', userRouter)
-
-app.listen(port, () => console.log('server en http://localhost:' + port))
+app.use('/.netlify/functions/api', movieRouter)
+app.use('/.netlify/functions/api', userRouter)
 
 module.exports.handler = serverless(app)

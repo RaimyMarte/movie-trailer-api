bf4be06a820c5ecfe682a050e8862fb00c375226
Raimy Marte <raimy241@gmail.com>
2023-04-14 10:05:49 -0400

    netlify config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const movieRouter = require('./routers/movie')
 const userRouter = require('./routers/user')
 const cors = require('cors')
 
+const serverless = require('serverless-http')
+
 //Data Base
 require('./db/dbConnection')
 
@@ -12,7 +14,9 @@ const port = 3000
 //Config Express
 app.use(cors())
 app.use(express.json())
-app.use(movieRouter)
-app.use(userRouter)
+app.use('/.netlify/functions/movie', movieRouter)
+app.use('/.netlify/functions/user', userRouter)
 
 app.listen(port, () => console.log('server en http://localhost:' + port))
+
+module.exports.handler = serverless(app)
